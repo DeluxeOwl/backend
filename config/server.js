8391e2f7960afbca74754a2a6481ce8ab6405e24
DeluxeOwl <nospamplease0@protonmail.com>
2020-12-30 16:10:28 +0200

    .

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f21eb2e19f609f5276a644572d6695e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3c41926d-7847-4a75-8107-cf2c9a4de539'
+};
